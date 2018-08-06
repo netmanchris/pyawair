92a8ed556923da9c8c0c8062512853be0da2d858
netmanchris <chris_young@me.com>
2018-08-06 14:36:04 -0400

    Fixed Issue: #4 Commented out Print Statements #8 Pypi pointing to wrong Github URL
    General cleanup
    Modified to version 0.0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across test_machine.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.4',
+    version='0.0.5',
 
     description='Python binding for Awair Air Quality Device API',
     long_description=long_description,
