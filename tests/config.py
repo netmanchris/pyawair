5531c63213e4b9b637083be86f84876fcb86165b
danielsjf <jef.daniels1@gmail.com>
2018-08-25 23:05:54 +0200

    Add config for tests. Do never commit again.

---
+++ tests/config.py
@@ -0,0 +1,12 @@
+TEST_DEVICE_NAME = None
+TEST_TOKEN = None
+
+def get_test_token():
+    if TEST_TOKEN is None:
+        raise ValueError("Invalid token")
+    return TEST_TOKEN
+
+def get_test_device_name():
+    if TEST_DEVICE_NAME is None:
+        raise ValueError("Invalid token")
+    return TEST_DEVICE_NAME
