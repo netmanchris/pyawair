0de755e6ded5d0bd386911a4038f5688d9ad5da5
danielsjf <jef.daniels1@gmail.com>
2018-11-19 23:45:42 +0100

    Index the result queries dynamically

--- pyawair/objects.py
+++ pyawair/objects.py
@@ -109,19 +109,20 @@ class AwairDev:
             data: list = pyawair.data.get_15_min_average(self._auth, device_id=self._id,
                                                          device_type=self._type, limit=1)
 
+        components = [d['comp'] for d in data[-1]['sensors']]
         self._data['score'] = data[-1]['score']
         if "temp" in DEVICE_SENSORS[self._type]:
-            self._data['temp'] = data[-1]['sensors'][0]['value']
+            self._data['temp'] = data[-1]['sensors'][components.index('TEMP')]['value']
         if "humid" in DEVICE_SENSORS[self._type]:
-            self._data['humid'] = data[-1]['sensors'][1]['value']
+            self._data['humid'] = data[-1]['sensors'][components.index('HUMID')]['value']
         if "co2" in DEVICE_SENSORS[self._type]:
-            self._data['co2'] = data[-1]['sensors'][2]['value']
+            self._data['co2'] = data[-1]['sensors'][components.index('CO2')]['value']
         if "voc" in DEVICE_SENSORS[self._type]:
-            self._data['voc'] = data[-1]['sensors'][3]['value']
+            self._data['voc'] = data[-1]['sensors'][components.index('VOC')]['value']
         if "dust" in DEVICE_SENSORS[self._type]:
-            self._data['dust'] = data[-1]['sensors'][4]['value']
+            self._data['dust'] = data[-1]['sensors'][components.index('DUST')]['value']
         if "pm25" in DEVICE_SENSORS[self._type]:
-            self._data['pm25'] = data[-1]['sensors'][5]['value']
+            self._data['pm25'] = data[-1]['sensors'][components.index('PM25')]['value']
         if "pm10" in DEVICE_SENSORS[self._type]:
-            self._data['pm10'] = data[-1]['sensors'][6]['value']
+            self._data['pm10'] = data[-1]['sensors'][components.index('PM10')]['value']
         self._last_update = datetime.datetime.now()  # records the time of the last update
